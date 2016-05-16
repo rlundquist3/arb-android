b35e8ded400e334d015a038b0878cddd2cec659a
Riley Lundquist <rlundquist3@gmail.com>
2016-05-16 11:48:17 -0400

    map setup

---
+++ app/src/main/java/com/rileylundquist/arb/MapsActivity.java
@@ -0,0 +1,66 @@
+package com.rileylundquist.arb;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnCameraChangeListener {
+
+    private GoogleMap mMap;
+
+    private final LatLng ARB_CENTER = new LatLng(42.293469, -85.701);
+    private final LatLngBounds ARB_AREA = new LatLngBounds(new LatLng(42.285, -85.71), new LatLng(42.30, -85.69));
+    private final int DEFAULT_ZOOM = 15;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        mMap.addMarker(new MarkerOptions().position(ARB_CENTER).title("Arb Center"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ARB_CENTER, DEFAULT_ZOOM));
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition cameraPosition) {
+
+        // Reposition camera if leaving arb area
+        if (cameraPosition.target.latitude > ARB_AREA.northeast.latitude)
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(ARB_AREA.northeast.latitude, cameraPosition.target.longitude)));
+        else if (cameraPosition.target.latitude < ARB_AREA.southwest.latitude)
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(ARB_AREA.southwest.latitude, cameraPosition.target.longitude)));
+        if (cameraPosition.target.longitude > ARB_AREA.northeast.longitude)
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(cameraPosition.target.latitude, ARB_AREA.northeast.longitude)));
+        else if (cameraPosition.target.latitude < ARB_AREA.southwest.longitude)
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(cameraPosition.target.latitude, ARB_AREA.southwest.longitude)));
+    }
+}
